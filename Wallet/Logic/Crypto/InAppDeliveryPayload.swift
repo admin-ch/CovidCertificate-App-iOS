4b28a5d2ce1ffea52d548d2d63d2197d408fc742
Patrick Amrein <amrein@ubique.ch>
2021-06-16 14:44:02 +0200

    add api calls for inappdelivery add endpoints

--- Wallet/Logic/Crypto/InAppDeliveryPayload.swift
+++ Wallet/Logic/Crypto/InAppDeliveryPayload.swift
@@ -21,64 +21,19 @@ public class InAppDeliveryPayload: Codable {
     public init() {
         algorithm = "EC256"
     }
-
-    static func getRegisterPayload() -> InAppDeliveryPayload? {
-        let payload = InAppDeliveryPayload()
-        let code = Luhn.generateLuhnCode()
-        guard case let .success(privateKey) = Crypto.makeAndStoreKey(name: code),
-              case let .success(publicKey) = Crypto.getPublicKeyData(fromPrivateKeyReference: privateKey) else {
-            return nil
-        }
-
-        guard let (signaturePayload, signature) = getSignature(forAction: .Register, withCode: code) else {
-            return nil
-        }
-
-        payload.signature = signature
-        payload.signaturePayload = signaturePayload
-        payload.code = code
-        payload.publicKey = publicKey.base64EncodedString()
-
-        return payload
-    }
-
-    public static func getPayload(forAction action: InAppDeliveryAction, withCode code: String) -> InAppDeliveryPayload? {
-        if case .Register = action {
-            return getRegisterPayload()
-        }
-
-        let payload = InAppDeliveryPayload()
-
-        guard let (signaturePayload, signature) = getSignature(forAction: action, withCode: code) else {
-            return nil
-        }
-
-        payload.signature = signature
-        payload.signaturePayload = signaturePayload
-        payload.code = code
-
-        return payload
-    }
-
-    static func getSignature(forAction action: InAppDeliveryAction, withCode code: String) -> (String, String)? {
-        var signaturePayload = ""
-        switch action {
-        case .Register:
-            signaturePayload = "register:\(code):\(Int(Date().timeIntervalSince1970) * 1000)"
-        case .Get:
-            signaturePayload = "get:\(code):\(Int(Date().timeIntervalSince1970) * 1000)"
-        case .Delete:
-            signaturePayload = "delete:\(code):\(Int(Date().timeIntervalSince1970) * 1000)"
-        }
-        guard let data = signaturePayload.data(using: .utf8),
-              case let .success(privateKey) = Crypto.loadKey(name: code),
-              case let .success(signature) = Crypto.signData(data, withPrivateKey: privateKey) else { return nil }
-        return (signaturePayload, signature.base64EncodedString())
-    }
 }
 
-public enum InAppDeliveryAction {
-    case Register
-    case Get
-    case Delete
+public class InAppDeliveryCertificateBody: Codable {
+    var pdf: String
+    var cert: String
+}
+
+public class DecryptedCertificate {
+    var pdf: Data
+    var cert: Data
+
+    init(_ cert: Data, _ pdf: Data) {
+        self.pdf = pdf
+        self.cert = cert
+    }
 }
