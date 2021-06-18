d6f868794565eafb76e96b55920e2002f34b9c0b
Fabian Aggeler <aggeler@ubique.ch>
2021-06-18 13:20:43 +0200

    Remove "ThisDeviceOnly" to make sure DCCs can be backuped

--- Wallet/Logic/User/SecureStorage/Enclave.swift
+++ Wallet/Logic/User/SecureStorage/Enclave.swift
@@ -43,7 +43,7 @@ public enum Enclave {
             let access =
             SecAccessControlCreateWithFlags(
                 kCFAllocatorDefault,
-                kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+                kSecAttrAccessibleWhenUnlocked,
                 [.privateKeyUsage],
                 &error
             )
