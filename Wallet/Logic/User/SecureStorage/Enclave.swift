8f3a37333099275f9bda6fce88ebc16c592560b6
Marco Zimmermann <zimmermann@ubique.ch>
2021-06-23 09:04:54 +0200

    allows silent push to access transfer codes for requesting

--- Wallet/Logic/User/SecureStorage/Enclave.swift
+++ Wallet/Logic/User/SecureStorage/Enclave.swift
@@ -43,7 +43,7 @@ public enum Enclave {
             let access =
             SecAccessControlCreateWithFlags(
                 kCFAllocatorDefault,
-                kSecAttrAccessibleWhenUnlocked,
+                kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly,
                 [.privateKeyUsage],
                 &error
             )
