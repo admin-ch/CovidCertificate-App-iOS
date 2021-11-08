75b313c3ef8a56cfb87b8fd9074c1cccbdc0cbce
Matthias Felix <felix@ubique.ch>
2021-11-08 11:31:47 +0100

    Refactor scan counting logic

--- Wallet/Screens/Certificates/WalletScannerViewController.swift
+++ Wallet/Screens/Certificates/WalletScannerViewController.swift
@@ -261,26 +261,11 @@ class WalletScannerViewController: ViewController {
     // MARK: - Many scans warning
 
     private func showTooManyScansWarningIfNecessary() {
-        if shouldShowTooManyScansWarning(newestScanDate: Date()) {
+        if WalletScanCounter.shouldShowTooManyScansWarning() {
             tooManyScansPopupView.presentFrom(view: view)
-            // Reset last scan dates whenever the popup is shown
-            WalletUserStorage.shared.lastScanDates = []
+            // Reset scan count whenever the popup is shown
+            WalletScanCounter.resetScanCount()
         }
-    }
-
-    private func shouldShowTooManyScansWarning(newestScanDate: Date) -> Bool {
-        // Add the latest scan date. We only ever consider the 10 latest scan dates
-        var dates = WalletUserStorage.shared.lastScanDates.suffix(9)
-        dates.append(newestScanDate)
-        WalletUserStorage.shared.lastScanDates = Array(dates)
-
-        // If there are less than 10 scan dates, don't show the warning
-        if dates.count < 10 {
-            return false
-        }
-
-        // If 10 scans have happened within 24 hours, show the warning
-        return dates.last!.timeIntervalSince(dates.first!) < 60 * 60 * 24
     }
 
     deinit {
@@ -311,6 +296,7 @@ extension WalletScannerViewController: QRScannerViewDelegate {
                 let cert = UserCertificate(qrCode: s, transferCode: nil)
                 detailViewController.certificate = cert
                 showDetail()
+                WalletScanCounter.trackScan(date: Date())
                 showTooManyScansWarningIfNecessary()
 
             case let .failure(error):
