e096fffe397b663635ab3022649fe4cb189d5243
Matthias Felix <felix@ubique.ch>
2021-11-16 13:53:57 +0100

    Fix possible retain cycles in certificate detail view

--- Wallet/Screens/Certificates/CertificateDetailViewController.swift
+++ Wallet/Screens/Certificates/CertificateDetailViewController.swift
@@ -255,6 +255,8 @@ class CertificateDetailViewController: ViewController {
     // MARK: - Check
 
     private func startTemporaryCheck() {
+        guard let qrCode = certificate?.qrCode else { return }
+
         temporaryVerifierState = .verifying
         state = .loading
         UIView.animate(withDuration: 0.2) {
@@ -262,20 +264,20 @@ class CertificateDetailViewController: ViewController {
             self.qrCodeStateView.state = self.temporaryVerifierState
         }
 
-        guard let qrCode = certificate?.qrCode else { return }
-
         VerifierManager.shared.addObserver(self, for: qrCode, forceUpdate: true) { [weak self] state in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                guard let strongSelf = self else { return }
+            guard let self = self else { return }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [weak self] in
+                guard let self = self else { return }
                 switch state {
-                case .loading: strongSelf.temporaryVerifierState = .verifying
-                case .skipped: strongSelf.temporaryVerifierState = .idle
-                case let .success(validUntil, _): strongSelf.temporaryVerifierState = .success(validUntil)
-                case .invalid: strongSelf.temporaryVerifierState = .failure
-                case let .retry(error, errorCodes): strongSelf.temporaryVerifierState = .retry(error, errorCodes)
+                case .loading: self.temporaryVerifierState = .verifying
+                case .skipped: self.temporaryVerifierState = .idle
+                case let .success(validUntil, _): self.temporaryVerifierState = .success(validUntil)
+                case .invalid: self.temporaryVerifierState = .failure
+                case let .retry(error, errorCodes): self.temporaryVerifierState = .retry(error, errorCodes)
                 }
 
-                strongSelf.state = state
+                self.state = state
             }
         }
     }
@@ -322,7 +324,9 @@ class CertificateDetailViewController: ViewController {
 
             // PDF export is enabled for certificates that were issed by switzerland and have a valid signature
             if issuedBySwitzerland {
-                CovidCertificateSDK.Wallet.check(holder: holder, forceUpdate: false) { results in
+                CovidCertificateSDK.Wallet.check(holder: holder, forceUpdate: false) { [weak self] results in
+                    guard let self = self else { return }
+
                     switch results.signature {
                     case let .success(result) where result.isValid:
                         self.exportRow.isEnabled = true
