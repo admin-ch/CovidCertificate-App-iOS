3f0de03d3f3affda41f7e7283e10331b05aa825e
Matthias Felix <felix@ubique.ch>
2021-12-02 13:50:19 +0100

    Fix layout of trailing icon button if titleLabel has two lines

--- CovidCertificate/SharedUI/Controls/TrailingIconButton.swift
+++ CovidCertificate/SharedUI/Controls/TrailingIconButton.swift
@@ -26,7 +26,7 @@ class TrailingIconButton: Button {
     override var intrinsicContentSize: CGSize {
         var contentSize = super.intrinsicContentSize
 
-        contentSize.height = max(contentSize.height, 44)
+        contentSize.height = max(contentSize.height, 56)
 
         if let img = imageView?.image {
             contentSize.width += 2 * (img.size.width + 20)
@@ -40,8 +40,9 @@ class TrailingIconButton: Button {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        if let img = imageView?.image {
-            let contentWidth = (img.size.width + (titleLabel?.intrinsicContentSize.width ?? 0)) / 2
+        if let img = imageView?.image, let titleLabel = titleLabel {
+            let titleLabelWidth = titleLabel.sizeThatFits(CGSize(width: bounds.width - 2 * 20 - img.size.width, height: bounds.height)).width
+            let contentWidth = (img.size.width + titleLabelWidth) / 2
             let offset = contentWidth + bounds.width / 2 - img.size.width - 20
             imageEdgeInsets = UIEdgeInsets(top: 0, left: offset, bottom: 0, right: -offset)
             titleEdgeInsets = UIEdgeInsets(top: 0, left: -img.size.width / 2, bottom: 0, right: img.size.width / 2)
