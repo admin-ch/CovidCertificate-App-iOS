2bb70a9f0d1289d5814e4ccd3b71ac30ddb274a4
Dennis Jüni <jueni@ubique.ch>
2021-12-08 17:35:20 +0100

    add images and new infoBox

---
+++ CovidCertificate/SharedUI/Views/IconTextInfoBoxView.swift
@@ -0,0 +1,77 @@
+//
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Foundation
+
+class IconTextInfoBoxView: PopupView {
+    private let stackView = UIStackView()
+
+    private let titleLabel = Label(.title, textAlignment: .center)
+
+    private let iconTextSource: [(String, String)]
+
+    private var closeButtonView = UIView()
+    private let closeButton = Button(title: UBLocalized.close_button, style: .text(.cc_blue))
+
+    init(iconTextStrs: [(String, String)]) {
+        iconTextSource = iconTextStrs
+        super.init()
+    }
+
+    override internal func setup() {
+        super.setup()
+
+        contentView.backgroundColor = .cc_white
+        contentView.layer.cornerRadius = 20.0
+
+        addSubview(contentView)
+
+        contentView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(Padding.large)
+            make.centerY.equalToSuperview()
+            make.top.greaterThanOrEqualToSuperview().inset(2.0 * Padding.large)
+            make.bottom.lessThanOrEqualToSuperview().inset(2.0 * Padding.large)
+        }
+
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview().inset(Padding.large)
+            make.bottom.equalToSuperview().inset(Padding.medium)
+        }
+
+        stackView.axis = .vertical
+
+        titleLabel.text = UBLocalized.accessibility_info_box
+
+        stackView.addArrangedView(titleLabel)
+        stackView.addSpacerView(Padding.medium + Padding.small - 2.0)
+
+        iconTextSource.forEach {
+            let icon = UIImage(named: $0.0)?.ub_image(with: .cc_blue)
+
+            stackView.addArrangedSubview(OnboardingInfoView(icon: icon, text: $0.1, alignment: .natural, leftRightInset: 0))
+        }
+
+        closeButtonView.addSubview(closeButton)
+        closeButton.snp.makeConstraints { make in
+            make.top.bottom.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview()
+            make.right.lessThanOrEqualToSuperview()
+        }
+
+        stackView.addArrangedSubview(closeButtonView)
+
+        closeButton.touchUpCallback = { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.dismiss()
+        }
+    }
+}
